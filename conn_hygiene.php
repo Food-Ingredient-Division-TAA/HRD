d52403c35ac8a9aa4167fb7d5a61542f61dc521c
icbpfidtaa <candra.adiwinata@icbp.indofood.co.id>
2023-08-09 15:54:59 +0700

    Create conn_hygiene.php

---
+++ conn_hygiene.php
@@ -0,0 +1,19 @@
+<?php
+//variabel koneksi
+$hygiene_conn = mysqli_connect("localhost","root","@candra27","hygiene");
+
+if(!$hygiene_conn){
+	echo "Koneksi Database Gagal...!!!";
+}
+$timeout = 45; // setting timeout dalam menit
+$logout = "login.php"; // redirect halaman logout
+$timeout = $timeout * 60; // menit ke detik
+if(isset($_SESSION['start_session'])){
+$elapsed_time = time()-$_SESSION['start_session'];
+   if($elapsed_time >= $timeout){
+   session_destroy();
+   echo "<script type='text/javascript'>alert('Sesi telah berakhir');window.location='$logout'</script>";
+   }
+}
+
+?>
