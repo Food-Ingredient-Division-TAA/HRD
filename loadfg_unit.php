f00ebfb91a5bcd591dc053e7c7770107dc481a13
icbpfidtaa <candra.adiwinata@icbp.indofood.co.id>
2023-08-09 16:51:20 +0700

    Create loadfg_unit.php

---
+++ loadfg_unit.php
@@ -0,0 +1,22 @@
+<?php
+include_once("connect_finger.php");
+include_once("koneksi.php");
+$day = date('Y-m-d');
+$fg_query = sqlsrv_query($connfinger, "SELECT TOP (1) [PersonalLogID]
+,[TerminalID]
+,[FingerPrintID]
+,[DateLog]
+,[TimeLog]
+,[VerifyMode]
+,[FunctionKey]
+,[Edited]
+,[UserName]
+,[FlagAbsence]
+,[DateTime]
+,[EmployeeStatus]
+,[FunctionKeyEdited]
+,[PersonalLogStatus] FROM [HITFPTA1].[dbo].[PersonalLog] ORDER BY PersonalLogID DESC");
+$datafg=sqlsrv_fetch_array($fg_query);
+$querysql = mysqli_query($konek, "SELECT * FROM karyawan WHERE kode_fg = '$datafg[FingerPrintID]'"); $data=mysqli_fetch_array($querysql);
+if(isset($data['unit'])){ echo $data['unit'];}else{ echo "";}
+?>
